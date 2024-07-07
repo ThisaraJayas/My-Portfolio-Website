42ed3cde7c933d7a42ea80a1fb446458b766a8e6
ThisaraJ <124505409+ThisaraJayas@users.noreply.github.com>
2024-07-07 19:44:40 +0530

    Update Portfolio_website.py

--- Portfolio_website.py
+++ Portfolio_website.py
@@ -102,12 +102,12 @@ with excellent teamwork, leadership, time management and communication abilities
             """, unsafe_allow_html=True)
 
 with colm2:
-    image_path = 'images/me1.png'
-    original_image = Image.open(image_path)
-    width, height = original_image.size
-    resized_image = original_image.resize((width // 2, height // 2))
-    st.image(resized_image)
-    ##st_lottie(lottie_animation)
+    # image_path = 'images/me1.png'
+    # original_image = Image.open(image_path)
+    # width, height = original_image.size
+    # resized_image = original_image.resize((width // 2, height // 2))
+    # st.image(resized_image)
+    st_lottie(lottie_animation)
 
 st.markdown('---')
 
